aa3377bcabff61edf5f615aebb8c08232db31363
Dani Ferri <daniferrito@hotmail.com>
2020-01-16 21:30:42 +0100

    Ads test coverage
    Closes #2

---
+++ src/pages/Home.test.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import Home from "./Home";
+import renderer from "react-test-renderer";
+
+afterEach(cleanup);
+
+test("renders without crashing", () => {
+  const { baseElement } = render(<Home />);
+  expect(baseElement).toBeDefined();
+});
+
+test("matches the snapshots", () => {
+  const component = renderer.create(<Home />);
+  expect(component).toMatchSnapshot();
+  const component2 = renderer.create(<Home thing="place" />);
+  expect(component2).toMatchSnapshot();
+});
+
+test("sets the text to the right thing", () => {
+  const component = render(<Home />);
+  expect(component.queryByText(/oyster/)).toBeTruthy();
+  const component2 = render(<Home thing="place" />);
+  expect(component2.queryByText(/place/)).toBeTruthy();
+});
